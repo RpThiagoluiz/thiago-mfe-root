2b85631e2559d1e7d78f7dff546ee92f124b1ed7
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2022-05-13 23:45:02 -0300

    feat: singleSpa on

--- src/mfethiago-root-config.ts
+++ src/mfethiago-root-config.ts
@@ -9,12 +9,25 @@ import microfrontendLayout from "./microfrontend-layout.html";
 const routes = constructRoutes(microfrontendLayout);
 const applications = constructApplications({
   routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
+  loadApp: ({ name }) => System.import(name),
 });
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
+registerApplication({
+  name: "@thiagoreact/thiagoreactone",
+  app: () => System.import("@thiagoreact/thiagoreactone"),
+  activeWhen: ["/"],
+});
+
 applications.forEach(registerApplication);
 layoutEngine.activate();
-start();
+
+start({
+  urlRerouteOnly: true,
+});
+
+// System.import("@thiagoreact/thiagoreactone").then(() => {
+//   // Activate the layout engine once the styleguide CSS is loaded
+//   layoutEngine.activate();
+//   start();
+// });
